ea981e28f4a061dc2b6dd535d008f63efbbf71c3
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2023-12-21 11:33:00 +0530

    Update launch.json, build.gradle, api_consts.dart, and services.dart files

--- lib/constants/api_consts.dart
+++ lib/constants/api_consts.dart
@@ -1,2 +1,2 @@
 String BASE_URL = "https://api.openai.com/v1";
-String API_KEY = "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";
+String API_KEY = "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";
